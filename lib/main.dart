0cc1fe5a587bcfcde3b3b1d92047f01cde4bb3ce
Tim Leung <timhk.leung@gmail.com>
2021-04-06 16:24:00 +0800

    RSS Bulletin Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
-import 'dart:typed_data';
+
 
 import 'package:flutter/material.dart';
 import 'package:device_apps/device_apps.dart';
 import 'dart:async';
 import 'package:intl/intl.dart';
 import 'package:splashscreen/splashscreen.dart';
+
+import 'package:wiserphonelauncher/rss.dart';
+
+
+
+
 
 void main() => runApp(MyApp());
 
@@ -91,15 +97,14 @@ class _ShowClockState extends State<ShowClock>
 }
 
 
+
 class Homepage extends StatefulWidget{
   @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-
   BouncingScrollPhysics _bouncingScrollPhysics = BouncingScrollPhysics();
-
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -337,7 +342,103 @@ class _HomepageState extends State<Homepage> {
                   ]
               ),
             ),
+            Container(
+              child: Table(
+                  border: TableBorder.all(width:0.0 ,style: BorderStyle.none   ),
+                  defaultVerticalAlignment: TableCellVerticalAlignment.top,
+                  children: <TableRow>[
+                    TableRow(
+                      children: <Widget>[
+                        Container(
+                          height: (MediaQuery.of(context).size.height) * 0.26 ,
+                          width: (MediaQuery.of(context).size.width),
+                          child: Center(
+                            child: ShowClock(),
 
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: <Widget>[
+                        Container(
+                          height: (MediaQuery.of(context).size.height) * 0.60 ,
+                          width: (MediaQuery.of(context).size.width),
+                          child: Center(
+                            child: new RSSFeed(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: <Widget>[
+                        Container(
+                          height: (MediaQuery.of(context).size.height) * 0.24,
+                          width: (MediaQuery.of(context).size.width),
+                          color: Colors.grey.withOpacity(0.3),
+                          child: FutureBuilder(
+                            future: DeviceApps.getInstalledApplications(
+                              includeSystemApps: true,
+                              onlyAppsWithLaunchIntent: true,
+                              includeAppIcons: true,
+                            ),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState==ConnectionState.done){
+                                List<Application> allapps=[];
+                                List<Application> loadedAllapps=snapshot.data;
+                                for (Application appitem in loadedAllapps){
+                                  if (appitem.packageName.contains("dialer") || appitem.packageName.contains("contacts") ||
+                                      appitem.packageName.contains("email") || appitem.packageName.contains("jelly")){
+                                    allapps.add(appitem);
+                                  }
+                                }
+                                return GridView.count(
+                                  crossAxisCount: 4,
+                                  padding: EdgeInsets.only(
+                                    top: 5.0,
+                                  ),
+                                  physics: _bouncingScrollPhysics,
+                                  children: List.generate(allapps.length,(index){
+                                    return GestureDetector(
+                                      onTap: () {
+                                        DeviceApps.openApp(allapps[index].packageName);
+                                      },
+                                      child: Column(
+                                        children: [
+                                          Image.memory(
+                                            (allapps[index] as ApplicationWithIcon).icon,
+                                            width:48,
+                                          ),
+                                          SizedBox(height:3.0,),
+                                          Text("${allapps[index].appName}",
+                                            style:TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                            overflow: TextOverflow.ellipsis, //if name too long, add .....
+                                          )
+                                        ],
+                                      ),
+                                    );
+
+                                  }),
+                                );
+                              }
+                              return Container(
+                                child: Center(
+                                  child: CircularProgressIndicator(),
+                                ),
+                              );
+                            },
+                          ),
+
+                          //child: FutureBuilder(), //PHONE, CALENDAR,BROWSER,EMAIL
+                        ),
+                      ],
+                    ),
+
+                  ]
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/rss.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:wiserphonelauncher/colors.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: new RSSFeed(),
+    );
+  }
+}
+
+class RSSFeed extends StatefulWidget {
+  RSSFeed({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _RSSFeedState createState() => _RSSFeedState();
+}
+
+
+class _RSSFeedState extends State<RSSFeed> {
+
+  //static const String FEED_URL = 'https://hnrss.org/jobs';
+
+  // HONG KONG
+  static const String FEED_URL='https://hk.news.yahoo.com/rss/hong-kong';
+
+  // JAPAN
+  //static const String FEED_URL='https://news.yahoo.co.jp/rss/topics/top-picks.xml';
+
+  RssFeed _feed; // RSS Feed Object
+  // TODO 5: Create a place holder for our title.
+  String _title; // Place holder for appbar title.
+
+  static const String loadingMessage = 'Loading Feed...';
+  static const String feedLoadErrorMessage = 'Error Loading Feed.';
+  static const String feedOpenErrorMessage = 'Error Opening Feed.';
+
+  GlobalKey<RefreshIndicatorState> _refreshKey;
+
+  updateFeed(feed) {
+    setState(() {
+      _feed = feed;
+    });
+  }
+
+
+  updateTitle(title) {
+    setState(() {
+      _title = title;
+    });
+  }
+
+  Future<void> openFeed(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: false,
+      );
+      return;
+    }
+    updateTitle(feedOpenErrorMessage);
+  }
+
+  load() async {
+    updateTitle(loadingMessage);
+    loadFeed().then((result) {
+      if (null == result || result.toString().isEmpty) {
+        // Notify user of error.
+        updateTitle(feedLoadErrorMessage);
+        return;
+      }
+      // If there is no error, load the RSS data into the _feed object.
+      updateFeed(result);
+      // Reset the title.
+      updateTitle("<Hacker News\\> | Jobs Feed");
+    });
+  }
+
+  Future<RssFeed> loadFeed() async {
+    try {
+      final client = http.Client();
+      final response = await client.get(FEED_URL);
+      print("The feed is loaded");
+      print(response.body);
+      return RssFeed.parse(response.body);
+    } catch (e) {
+      // handle any exceptions here
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshKey = GlobalKey<RefreshIndicatorState>();
+    updateTitle(widget.title);
+    load();
+  }
+
+  // TODO 14: Create a method to check if the RSS feed is empty.
+  // Method to check if the RSS feed is empty.
+  isFeedEmpty() {
+    return null == _feed || null == _feed.items;
+  }
+
+  // TODO 15: Create method to load the UI and RSS data.
+  // Method for the pull to refresh indicator and the actual ListView UI/Data.
+  body() {
+    return isFeedEmpty()
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : RefreshIndicator(
+      key: _refreshKey,
+      child: list(),
+      onRefresh: () => load(),
+    );
+  }
+
+  list() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children: <Widget>[
+          // Container displaying RSS feed info.
+         /* Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.only(left: 5.0, right: 5.0),
+              decoration: customBoxDecoration(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Link: " + _feed.link,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: colorHackerHeading),
+                  ),
+                  Text(
+                    "Description: " + _feed.description,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: colorHackerHeading),
+                  ),
+                  Text(
+                    "Last Build Date: " + _feed.lastBuildDate,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: colorHackerHeading),
+                  ),
+                ],
+              ),
+            ),
+          ),
+         */ // ListView that displays the RSS data.
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: ListView.builder(
+                padding: EdgeInsets.all(5.0),
+                itemCount: _feed.items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = _feed.items[index];
+                  return Container(
+                    margin: EdgeInsets.only(
+                      bottom: 10.0,
+                    ),
+                    decoration: customBoxDecoration(),
+                    child: ListTile(
+                      title: title(item.title),
+                      subtitle: subtitle(item.pubDate.toString()),
+                      trailing: rightIcon(),
+                      contentPadding: EdgeInsets.all(5.0),
+                      onTap: () => openFeed(item.link),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ]);
+  }
+
+  // Method that returns the Text Widget for the title of our RSS data.
+  title(title) {
+    return Text(
+      title,
+      style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.w500,
+          color: colorHackerHeading),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  // Method that returns the Text Widget for the subtitle of our RSS data.
+  subtitle(subTitle) {
+    return Text(
+      subTitle,
+      style: TextStyle(
+          fontSize: 15.0,
+          fontWeight: FontWeight.w300,
+          color: colorHackerHeading),
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  // Method that returns Icon Widget.
+  rightIcon() {
+    return Icon(
+      Icons.keyboard_arrow_right,
+      color: colorHackerBorder,
+      size: 30.0,
+    );
+  }
+
+  // Custom box decoration for the Container Widgets.
+  BoxDecoration customBoxDecoration() {
+    return BoxDecoration(
+      border: Border.all(
+       // color: colorHackerBorder, // border color
+        width: 0.0,
+      ),
+      color: Colors.grey.withOpacity(0.2),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        // Set the transparency here
+        canvasColor: Colors.white70.withOpacity(0.3), //or any other color you want. e.g Colors.blue.withOpacity(0.5)
+      ),
+        child: Container(
+          child:Center(
+            child: body(),
+          ),
+        )
+    );
+  }
+}
